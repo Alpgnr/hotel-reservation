bd0f4c1fa34ea1a2b2786e6f26e2384c64568cd6
Alpgnr <alpguner02@gmail.com>
2026-02-20 22:24:08 +0300

    feat: created seeding function and changed types

--- backend/seed_rooms.js
+++ backend/seed_rooms.js
@@ -2,16 +2,30 @@ require('dotenv').config();
 const db = require("./db");
 
 const types = [
-  { type: "Single", capacity: 1, image: "single.jpg" },
-  { type: "Double", capacity: 2, image: "double.jpg" },
-  { type: "Luxury", capacity: 2, image: "luxury.jpg" },
-  { type: "Family", capacity: 4, image: "family.jpg" },
-  { type: "Suite", capacity: 3, image: "suite.jpg" }
+  { type: "Single", capacity: 1, image: "single.jpg", price: 4000 },
+  { type: "Double", capacity: 2, image: "double.jpg", price: 5000 },
+  { type: "Luxury", capacity: 2, image: "luxury.jpg", price: 6000 },
+  { type: "Family", capacity: 4, image: "family.jpg", price: 7000 },
+  { type: "Suite", capacity: 3, image: "suite.jpg", price: 10000 }
 ];
 
 async function seedRooms() {
-  
-  process.exit(0);
-}
+  try {
+    for (let i = 0; i < types.length; i++) {
+      const room = types[i];
+      const room_number = 101 + i;
+
+      await db.query(
+        "INSERT INTO rooms (room_number, price, capacity, type, image) VALUES (?, ?, ?, ?, ?)",
+        [room_number, room.price, room.capacity, room.type, room.image]
+      );
+      
+      console.log(`Oda eklendi: ${room_number} - ${room.type} (Resim: ${room.image})`);
+    }
+  } catch (error) {
+    console.log("\n Toplam 5 farklı oda tipi başarıyla oluşturuldu.");
+    process.exit(0);
+  };
+};
 
 seedRooms().catch(e => { console.error(e); process.exit(1); });
